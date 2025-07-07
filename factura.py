07a2766d7be30f15f05d6d83160c020c22e2b25e
kayyrod21 <kaylee.rodriguez@live.com>
2025-07-06 20:39:56 -0400

    Agregar función para calcular total con IVA

---
+++ factura.py
@@ -0,0 +1,7 @@
+git init
+git add factura.py
+git commit -m "Agregar función para calcular total con IVA"
+git branch -M main
+git remote add origin https://github.com/tu-usuario/factura-con-iva.git
+git push -u origin main
+
